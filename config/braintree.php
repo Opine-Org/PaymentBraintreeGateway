57657d1e32ff2235abf9309eba5f50ad517f367c
webuser <ryan@virtuecenter.com>
2014-03-06 17:23:19 -0500

    set sandbox credentials

--- config/braintree.php
+++ config/braintree.php
@@ -1,2 +1,7 @@
 <?php
-return [];
+return [
+	'MerchantID' => 's7p5b9dtbv7cnzk2',
+	'PublicKey' => 'bjmhkzty448dzd3t',
+	'PrivateKey' => 'c1fbf8d697e5fa8d0c20557601d08501',
+	'CSE' => 'MIIBCgKCAQEA17UBn7jHupC/HFcrbUVkJT45UUBNHpjmaNYDEtCboaFfJI+YtX6Rn10M7gZ7HRwDJepIOHYzWZFNGsKvt9DNY+m7d8H9xYynB/jIO93O3CqPRut7Huts93bvyASZWRjdKgTMJQna4U0iI4s/u/6eykV0vjqZJCvCPna6hFTSRajJOhSqzafdP05KUFxoaQnEUzNZ2twF9eYQRJ42bK77X6AXfz9lhvT+qOT5nwSJau/ovaSsv8JJPE4rTBw5369E85N/+XWrd954kiyFDVIDkoB9V8L24kFCb25FRSDNYPgTZS6QrY5jpExDqebxftd+bLbrbskYtcScFczNJU14TQIDAQAB'
+];
