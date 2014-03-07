f6edbbf5e3e0d325a2c1c76a43d9771879f1d18a
webuser <ryan@virtuecenter.com>
2014-03-06 19:28:07 -0500

    refactored, more tests

--- src/PaymentBraintreeGateway.php
+++ src/PaymentBraintreeGateway.php
@@ -4,46 +4,69 @@ namespace Opine;
 class PaymentBraintreeGateway {
 	private $config;
 	private $db;
-	private $MerchantID;
-	private $PublicKey;
-	private $PrivateKey;
-	private $CSE;
-	private $Environment;
 
 	public function __construct ($db, $config) {
 		$this->db = $db;
 		$this->config = $config;
 		$braintreeConfig = $this->config->braintree;
-		$this->MerchantID = $braintreeConfig['MerchantID'];
-		$this->PublicKey = $braintreeConfig['PublicKey'];
-		$this->PrivateKey = $braintreeConfig['PrivateKey'];
-		$this->CSE = $braintreeConfig['CSE'];
-		$this->Environment = $braintreeConfig['Environment'];
+		\Braintree_Configuration::environment($braintreeConfig['Environment']);
+		\Braintree_Configuration::merchantId($braintreeConfig['MerchantID']);
+		\Braintree_Configuration::publicKey($braintreeConfig['PublicKey']);
+		\Braintree_Configuration::privateKey($braintreeConfig['PrivateKey']);
 	}
 
-	public function payment ($description, $amount, array $paymentInfo, array $billingInfo) {
-		\Braintree_Configuration::environment($this->Environment);
-		\Braintree_Configuration::merchantId($this->MerchantID);
-		\Braintree_Configuration::publicKey($this->PublicKey);
-		\Braintree_Configuration::privateKey($this->PrivateKey);
+	public function payment ($description, $amount, array $paymentInfo, array $billingInfo, &$response) {
+		if (empty($description)) {
+			throw new \Exception('Invalid Description');
+		}
+		if (!is_numeric($amount) || $amount === 0) {
+			throw new \Exception('Invalid Amount');
+		}
+		if (!isset($paymentInfo['number'])) {
+			throw new \Exception('Card Number not set');
+		}
+		if (!isset($paymentInfo['expiration'])) {
+			throw new \Exception('expiration');
+		}
+		$cvv = null;
+		if (isset($paymentInfo['cvv'])) {
+			$cvv = $paymentInfo['cvv'];
+		}
 
 		$result = \Braintree_Transaction::sale(array(
-		    'amount' => '1000.00',
+		    'amount' => $amount,
 		    'creditCard' => array(
-		        'number' => '5105105105105100',
-		        'expirationDate' => '05/12'
+		        'number' => $paymentInfo['number'],
+		        'expirationDate' => $paymentInfo['expiration']
 		    )
 		));
-
 		if ($result->success) {
-		    print_r("success!: " . $result->transaction->id);
+		    $response = [
+		    	'success' => true,
+		    	'transaction_id' => $result->transaction->id,
+		    	'response' => $result->transaction
+		    ];
+		    return true;
 		} else if ($result->transaction) {
-		    print_r("Error processing transaction:");
-		    print_r("\n  code: " . $result->transaction->processorResponseCode);
-		    print_r("\n  text: " . $result->transaction->processorResponseText);
-		} else {
-		    print_r("Validation errors: \n");
-		    print_r($result->errors->deepAll());
+			$response = [
+		    	'success' => false,
+		    	'errorMessage' => $result->transaction->processorResponseText,
+		    	'errorCode' => $result->transaction->processorResponseCode,
+		    	'response' => $result->transaction
+		    ];
+		    return false;
 		}
+		$error = $result->errors->deepAll()[0];
+		$response = [
+		    'success' => false,
+			'errorMessage' => $error->message,
+			'errorCode' => $error->code,
+			'response' => $error
+		];
+		return false;
+	}
+
+	private function saveTransactions () {
+
 	}
 }
--- tests/PaymentBraintreeGatewayTest.php
+++ tests/PaymentBraintreeGatewayTest.php
@@ -13,7 +13,52 @@ class PaymentBraintreeGatewayTest extends \PHPUnit_Framework_TestCase {
         $this->db = $container->db;
     }
 
+    public function testPaymentInvalidDescription() {
+        $response = [];
+        $correctError = false;
+        try {
+            $this->paymentGateway->payment('', 9, [], [], $response);
+        } catch (\Exception $e) {
+            if ($e->getMessage() == 'Invalid Description') {
+                $correctError = true;
+            }
+        }
+        $this->assertTrue($correctError);
+    }
+
+    public function testPaymentInvalidAmount () {
+        $response = [];
+        $correctError = false;
+        try {
+            $this->paymentGateway->payment('12 eggs', 'Five', [], [], $response);
+        } catch (\Exception $e) {
+            if ($e->getMessage() == 'Invalid Amount') {
+                $correctError = true;
+            }
+        }
+        $this->assertTrue($correctError);
+    }
+
     public function testPaymentSuccess () {
-    	$this->paymentGateway->payment('', 9, [], []);
+    	$response = [];
+        $result = $this->paymentGateway->payment('12 eggs', 9, [
+            'number' => '5105105105105100',
+            'expiration' => '05/12'
+        ], [], $response);
+        $this->assertTrue($result);
+    }
+
+    public function testPaymentFailureInvalidCardNumber () {
+        $response = [];
+        $result = $this->paymentGateway->payment('12 eggs', 9, [
+            'number' => '5105105105105199',
+            'expiration' => '05/12'
+        ], [], $response);
+        $this->assertFalse($result);
+        $correctError = false;
+        if ($response['errorMessage'] == 'Credit card number is invalid.') {
+            $correctError = true;
+        }
+        $this->assertTrue($correctError);
     }
 }
